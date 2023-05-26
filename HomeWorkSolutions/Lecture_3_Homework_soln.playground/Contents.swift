573fe7f97958909c78be99b4f6b5f25948527a01
Campbell, Christina <christinac9895@gmail.com>
2023-05-25 23:17:43 -0700

    HW Solutions

---
+++ HomeWorkSolutions/Lecture_3_Homework_soln.playground/Contents.swift
@@ -0,0 +1,131 @@
+
+/*
+ Problem Statement:
+ We will be practicing using collections, constants and variables, operators, optionals, and data types by going through a series of short questions surrounding a virtual classroom
+ */
+
+
+// Instructions: go through the following 6 mini problems, adding code after the blocks that read // Your code here:
+
+
+// 1. Define variables or constants for the first name, middle name, last name, and highest test score of the year so far for a sample student. Please assign values for these items.
+
+// Your code here:
+let firstName = "Alex"
+let middleName = "James"
+let lastName = "Rickman"
+var highestScoreSoFar = 93.0
+
+
+// 2. The student was able to get some extra credit! This allows them to increase their score value from part 1. by 20 percent! Please create a new variable storing the updated score, using the old variable to directly calculate the new score. Ex. if I have var score = 10, a 30 % increase would 1.3 * score.
+
+// Your code here:
+highestScoreSoFar *= 1.2
+
+
+
+// 3. Below is a set of class pet types allowed by the School Board. Please add one more type to the set.
+var allowedClassPets: Set<String> = ["Hamster", "Guinea Pig", "Frog", "Snake"]
+
+// Your code here:
+allowedClassPets.update(with: "Fish")
+
+
+// 4. Below are 3 students packaged into tuples with their name and test score. Create an array of Ints containing just the scores in descending order.
+let studentOne = (name: "Alex", score: 10)
+let studentTwo = (name: "Navdeep", score: 15)
+let studentThree = (name: "Christina", score: 20)
+
+
+// Your code here:
+let scores = [studentThree.score, studentTwo.score, studentOne.score]
+let fancyWayScore = [studentOne, studentTwo, studentThree].map {
+    $0.score
+}.sorted(by: >)
+
+
+// 5. Based on the scores from part 4., the students have been awarded grades. Alex received a C, Navdeep a B, and Christina an A. Create a dictionary storing Key-Values pairs of this data, with the key being the name and the value being the grade. Note: the grade must be of type Character for full credit.
+
+// Your code here:
+var grades: [String : Character] = ["Alex" : "C", "Christina" : "A", "Navdeep" : "B"]
+
+
+// 6. Navdeep got some extra credit! The teacher bumped up his grade in the class from a B to an A. Without creating another dictionary, edit the dictionary from part 5 to replace Navdeep's grade of B with an A
+
+// Your code here:
+grades["Navdeep"] = "A"
+
+
+
+
+
+// This assignment will take us through a hypothetical class arrangement, going through the core concepts of Control-Flow, Loops, and Functions.
+
+
+
+/* 1. Write a function that accepts the following parameters:
+1) a tuple of (String, Int) pairs representing a student name and their test score
+2) a threshold score that is needed to pass
+ 
+ Write the body of the function to return either "Pass" or "Fail" depending on whether the score is above the inputted threshold
+ 
+ 
+ Create 3 test inputs and call your function with them.
+ */
+
+
+// Your Code Below:
+
+func classStatus(student: (name: String, score: Int), threshold: Int) -> String {
+    if student.score >= threshold {
+        return "Pass"
+    }
+    return "Fail"
+}
+
+
+
+/*
+ 
+ 2. Write a function that accepts an array of Tuples of (String, Int) pairs representing a Name and Test Score of a student, as well as a score threshold, and returns an array of Strings representing the names of the students who passed.
+ 
+ Create 3 test inputs and call your function with them.
+ */
+
+// Your Code Below:
+
+func classStatusTwo(students: [(name: String, score: Int)], threshold: Int) -> [String] {
+    var passingStudents: [String] = []
+    for student in students {
+        if student.score >= threshold {
+            passingStudents.append(student.name)
+        }
+    }
+    return passingStudents
+}
+
+/*
+ 
+ 3. Write a function that accepts an array of Tuples of (String, Int) pairs representing a Name and Test Score of a student, as well as a score threshold. In the body of the function, write code to filter out tuple values of the input array where the scores were below the given input threshold. Return the filtered array.
+ 
+    Hint: use the built-in Array filter method.
+ 
+ Create 3 test inputs and call your function with them.
+ */
+
+
+// Your Code Below:
+
+func classStatusThree(students: [(name: String, score: Int)], threshold: Int) -> [(name: String, score: Int)] {
+    var passingStudents = students
+    passingStudents = passingStudents.filter { (name: String, score: Int) in
+        score >= threshold
+    }
+    return passingStudents
+}
+
+
+
+
+
+
