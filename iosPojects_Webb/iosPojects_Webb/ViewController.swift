c13533af5835fdd37faac199966a218d8157e458
Steven Webb <swebb9904@gmail.com>
2023-06-09 13:24:44 -0400

    First commit

---
+++ iosPojects_Webb/iosPojects_Webb/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iosPojects_Webb
+//
+//  Created by Steven Webb on 6/7/23.
+//
+
+import UIKit
+import Foundation
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var morgageCalculatorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    @IBAction  public func showNextScreen(_ sender: Any) {
+        
+    }
+
+}
+
