6dc4f5dd7b800c2937f826421e3f3c85c68cc902
Steven Webb <swebb9904@gmail.com>
2024-01-22 11:09:10 -0500

    first commit

---
+++ iosPojects_Webb/Hello Wordl/Hello Wordl/Core/KeyCell.swift
@@ -0,0 +1,51 @@
+//
+//  KeyCell.swift
+//  Hello Wordl
+//
+//  Created by Steven Webb on 7/12/23.
+//
+
+import UIKit
+
+class KeyCell: UICollectionViewCell {
+    static let identifier = "KeyCell"
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo:
+                contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo:
+                contentView.trailingAnchor),
+            label.topAnchor.constraint(equalTo:
+                contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo:
+                contentView.bottomAnchor),
+                                    
+         ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
+    
+    func configure(with letter: Character) {
+        label.text = String(letter).uppercased()
+    }
+}
--- iosPojects_Webb/MortagageCallculator/MortagageCallculator/ViewController.swift
+++ iosPojects_Webb/MortagageCallculator/MortagageCallculator/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var pleaseClickMe: UIButton!
+    @IBOutlet weak var enterNameHere: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         text.text = "Morgage Calculator"
     }
-
+    
+    @IBAction func pleaseClickMe(_sender: Any) {
+        
+        let alert = UIAlertController(title: "Congrats!", message: "You created an alert", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Close", style: .default)
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true)
+    }
 
 }
 
+    
+    
